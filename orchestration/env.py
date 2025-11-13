8087d85ce6d7a44fe812bd9471eaec914f3525cc
Vong Ly <vongly62@gmail.com>
2025-11-12 17:39:35 -0800

    adding full paths to env vars

--- orchestration/env.py
+++ orchestration/env.py
@@ -6,9 +6,7 @@ env_path = Path(__file__).resolve().parent / '.env'
 from dotenv import load_dotenv
 load_dotenv(dotenv_path=env_path)
 
-ROOT_DIR = Path(__file__).resolve().parent.parent
 
-
-PARSE_PDFS_S3_PATH = ROOT_DIR / os.getenv('PARSE_PDFS_S3_PATH')
-PARSE_PDFS_S3_PATH_TEST = ROOT_DIR / os.getenv('PARSE_PDFS_S3_PATH_TEST')
-PARSE_PDFS_S3_PYTHON_EXEC_PATH = ROOT_DIR / os.getenv('PARSE_PDFS_S3_PYTHON_EXEC_PATH')
+PARSE_PDFS_S3_PATH = os.getenv('PARSE_PDFS_S3_PATH')
+PARSE_PDFS_S3_PATH_TEST = os.getenv('PARSE_PDFS_S3_PATH_TEST')
+PARSE_PDFS_S3_PYTHON_EXEC_PATH = os.getenv('PARSE_PDFS_S3_PYTHON_EXEC_PATH')
